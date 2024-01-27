0538121807b874a334264482efa94f1904a80cd7
Youjin Tyan <102475316+YoujinTyan@users.noreply.github.com>
2024-01-27 14:36:42 +0600

    Update m2l15t8.py

--- tasks. part 2/task 8/m2l15t8.py
+++ tasks. part 2/task 8/m2l15t8.py
@@ -8,8 +8,8 @@ def print_primes(n):
     for num in range(2, n):
         prime = True
         for i in range(2, num):
-            if num % i == 0:
+            if num - i == 0:
                 prime = False
         if prime:
-            print(num)
+            print num
 
